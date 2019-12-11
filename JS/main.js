c4b338084e3451d42de4c216991a30be69edcb93
Bruno Vanderlinde <brunov.teixeira@hotmail.com>
2019-12-11 14:50:59 -0300

    Meu primeiro Javascript

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+var nome =" Bruno Vanderlinde";
+var n1 = 23;
+var n2 = 10;
+var frase = "Japão é o melhor time do mundo";
+//alert (nome +" tem"+ idade +  "anos");
+// alert (idade+idade2)
+console.log (nome);
+console.log (n1 * n2);
+console.log (frase.replace("melhor", "pior"));
+//alert (frase.replace ("Japão","Brasil"));
